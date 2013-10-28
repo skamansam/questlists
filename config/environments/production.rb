97cbe2fb83bfa47bc705921ff84fa22a43e50892
Samuel C. Tyler <skamansam@gmail.com>
2013-10-28 11:59:19 -0400

    added themes to precompile list.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Questlists::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( themes/amelia.css themes/cyborg.css themes/slate.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
