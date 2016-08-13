f4ef982f7f192401d3baee507801082b0e937b0d
Samuel C. Tyler <skamansam@gmail.com>
2016-08-13 10:54:42 -0400

    edit local firebase stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -43,13 +43,13 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
+    console.log('Questlists is ready to rock!');
   });
 
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
-    //console.log('SINGING IN')
+    console.log('Components are ready!')
     //Polymer.dom(document).querySelector('#auth').signInAnonymously();
   });
 
